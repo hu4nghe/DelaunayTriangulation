81250bcf22ef26585e52f1fe126ceb515f0eb4ec
HUANG He <hu4nghe@outlook.com>
2025-09-26 02:20:19 +0200

    add main delaunay logic

--- modules/delaunay/src/delaunay.cpp
+++ modules/delaunay/src/delaunay.cpp
@@ -9,22 +9,85 @@
  * 
  */
 #include "csv_parser.h"
+#include "geo_2D/triangle_2D.h"
 
+#include <ranges>
+#include <functional>
+#include <set>
 
-#include <print>
- 
+using namespace tools_2D;
+
+auto super_triangle(const std::vector<point>& points) 
+{
+    auto [min_x, max_x] = std::ranges::minmax(points | std::views::transform(std::mem_fn(&point::get_x)));
+    auto [min_y, max_y] = std::ranges::minmax(points | std::views::transform(std::mem_fn(&point::get_x)));
+
+    double avg_x = (min_x + max_x) / 2.0;
+    const point p1(avg_x, max_y);
+    const point p2(max_x, min_y);
+    double k = p1.slope(p2);
+
+    const point a(avg_x,                               max_y - k * avg_x);
+    const point b(max_x + (min_y - max_y - 1) / k + 1, min_y - 1);
+    const point c(2 * avg_x - b.get_x(),               min_y - 1); 
+
+    return std::tuple(a, b, c);
+}
+
+auto delaunay_triangulate(const std::vector<point>& points) 
+{
+    if (points.size() <= 3) 
+        throw std::invalid_argument("At least 4 points are required for triangulation.");
+
+    // Construct a super triangle at first.
+    auto [p1, p2, p3] = super_triangle(points);
+    std::vector<triangle> all_triangles{triangle(p1, p2, p3)};
+
+    for (const auto& p : points) 
+    {
+        std::set<segment> polygon;
+        std::erase_if(
+            all_triangles, 
+            [&](const triangle& tri)
+            {
+                if (tri.circum_circle().contains(p))
+                {
+                    for (const auto& e : tri.get_edges()) 
+                        if (!polygon.erase(e)) 
+                            polygon.insert(e);
+                    return true;
+                }
+                return false;             
+            });
+                         
+        //construct new triangles with current point and segments.                                
+        for (const auto& e : polygon)
+            all_triangles.emplace_back(p, e);
+            
+    }
+
+    // Remove triangles that contain any of the super triangle vertices.
+    std::erase_if(
+        all_triangles,
+        [&](const triangle& tri) 
+        {
+            for (const auto& p : {p1, p2, p3})
+                if(tri.containsVertex(p)) 
+                    return true;
+            return false;
+        });
+                                        
+    return all_triangles;
+}
+
 int main()
 {
-    try 
-    {
-        auto coords = read_csv_coords("points.csv");
-        
+    auto coords = read_csv_coords("points.csv");
+    std::vector<point> points;
+    for(const auto [x, y] : coords)
+        points.emplace_back(x, y);
 
-    }
-    catch (const parse_error& e) 
-    {
-        std::println(stderr, "Format error : {}", e.what());
-        return 1;
-    }
-    
+    auto result = delaunay_triangulate(points);
+
+    return 0;   
 }
